37c77cc77c9e906e23f68ced24ccf4df25af67bd
Zuger Cédric <zuger.cedric@gmail.com>
2018-01-06 15:33:29 +0100

    Server modifications

--- monitor.rb
+++ monitor.rb
@@ -5,15 +5,17 @@ require 'yaml'
 
 def send_warning_email( sitename, w_data )
   message = <<MESSAGE_END
-From: <#{w_data[:sender]}>
-To: <#{w_data[:watcher]}>
+From: <#{w_data['sender']}>
+To: <#{w_data['dest']}>
 Subject: Site failure
 
 #{sitename} n'est plus accessible.
 MESSAGE_END
 
+p w_data, message
+
   Net::SMTP.start('127.0.0.1') do |smtp|
-    smtp.send_message message, w_data[:sender], w_data[:watcher]
+    smtp.send_message message, w_data['sender'], w_data['dest']
   end
 end
 
@@ -32,7 +34,7 @@ File.open( 'sites.txt' ).readlines.each do |site|
 
   puts "#{site} : #{res.code}"
 
-  unless [ 200, 401 ].include?( res.code )
+  unless [ '200', '401' ].include?( res.code )
     send_warning_email( site, w_data )
   end
 
